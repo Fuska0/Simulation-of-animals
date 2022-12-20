3cb0df22149de46afc682ca9da5b407aba2b799d
Łukasz Stępień <uqsz123@gmail.com>
2022-12-20 20:49:58 +0100

    klasa GrassfieldForestedEquatoria

--- src/main/java/agh/cs/po/Classes/GrassfieldForestedEquatoria.java
+++ src/main/java/agh/cs/po/Classes/GrassfieldForestedEquatoria.java
@@ -1,5 +1,40 @@
 package agh.cs.po.Classes;
 
-public class GrassfieldForestedEquatoria {
+import java.util.HashMap;
+import java.util.concurrent.ThreadLocalRandom;
 
+public class GrassfieldForestedEquatoria extends AbstractWorldMap{ // zmienic nazwa na PlantsFieldForestedEquatoria
+    Parameters parameters = new Parameters();
+    protected HashMap<Vector2d,Plants> plantsHashMap = new HashMap<>();
+    private int n = parameters.plantsGrowingNumber;
+    private int a = parameters.mapHeight;
+    private int b = parameters.mapWidth;
+
+    public GrassfieldForestedEquatoria(int n){
+        generateGrass(n);
+    }
+    private void generateGrass(int n) {
+        for(int i = 0; i< this.n; i++) {
+            int x = ThreadLocalRandom.current().nextInt(0, a);
+            int y = ThreadLocalRandom.current().nextInt((int) Math.floor(b/3), (int) Math.floor(2*b/3));
+            if (isOccupied(new Vector2d(x, y))) {
+                i--;
+            }
+            else {
+                plantsHashMap.put(new Vector2d(x, y),new Plants(new Vector2d(x, y)));
+            }
+        }
+    }
+
+    @Override
+    public boolean isOccupied(Vector2d position) {
+        return plantsHashMap.containsKey(position);
+    }
+    @Override
+    public Object objectAt(Vector2d position) {
+        if (super.objectAt(position) == null) {
+            return plantsHashMap.get(position);
+        }
+        return super.objectAt(position);
+    }
 }
