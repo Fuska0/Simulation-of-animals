352b463a4c49c83d4c233a7e72d28423346ae17b
Łukasz Stępień <uqsz123@gmail.com>
2022-12-20 20:29:37 +0100

    klasa Plants

--- src/main/java/agh/cs/po/Classes/Plants.java
+++ src/main/java/agh/cs/po/Classes/Plants.java
@@ -1,5 +1,18 @@
 package agh.cs.po.Classes;
 
 public class Plants {
+    Parameters parameters = new Parameters();
+    private final Vector2d position;
+    private final int energy= parameters.plantEnergy;
+    public Plants(Vector2d position) {
+        this.position = position;
+    }
 
+    public Vector2d getPosition() {
+        return position;
+    }
+
+    public String toString(){
+        return "*";
+    }
 }
